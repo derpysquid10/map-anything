2b771e2b45e7372b361798d2bafba4dd02a674bf
Gordon <gordon.tan111@gmail.com>
2025-10-29 18:02:36 +0000

    addpow3r_vggt eval bash

---
+++ bash_scripts/benchmark/dense_n_view/pow3r_vggt_multi.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the Apache License, Version 2.0
+# found in the LICENSE file in the root directory of this source tree.
+
+export HYDRA_FULL_ERROR=1
+
+# Define the batch sizes and number of views to loop over
+batch_sizes_and_views=(
+    "10 2 benchmark_518_eth3d_snpp_tav2"
+    "10 4 benchmark_518_eth3d_snpp_tav2"
+    "10 8 benchmark_518_eth3d_snpp_tav2"
+    "5 16 benchmark_518_eth3d_snpp_tav2"
+    "4 24 benchmark_518_eth3d_snpp_tav2"
+    # "2 32 benchmark_518_eth3d_snpp_tav2"
+    # "1 50 benchmark_518_eth3d_snpp_tav2"
+    # "1 100 benchmark_518_eth3d_snpp_tav2"
+)
+
+# Loop through each combination
+for combo in "${batch_sizes_and_views[@]}"; do
+    # Split the string into batch_size and num_views
+    read -r batch_size num_views dataset <<< "$combo"
+
+    echo "Running $dataset with batch_size=$batch_size and num_views=$num_views"
+
+    python3 \
+        benchmarking/dense_n_view/benchmark.py \
+        machine=default \
+        dataset=$dataset \
+        dataset.num_workers=12 \
+        dataset.num_views=$num_views \
+        batch_size=$batch_size \
+        model=pow3r_vggt \
+        amp=0 \
+        model.model_config.load_custom_ckpt=true \
+        model.model_config.custom_ckpt_path="/work/weights/pow3r_vggt/multi_6D_KRay_argoverse/checkpoint_0.pt" \
+        +model/task=nmp \
+        hydra.run.dir='${root_experiments_dir}/mapanything/benchmarking/dense_'"${num_views}"'_view/pow3r_vggt_multi'
+
+    echo "Finished running $dataset with batch_size=$batch_size and num_views=$num_views"
+done
